7ab0f09072914336ac55034b3be4791ba6386b59
Alexey Chugunov <chugunov.alexey@gmail.com>
2019-05-12 18:46:50 +0300

    rename

---
+++ webserver.py
@@ -0,0 +1,32 @@
+#import SimpleHTTPServer
+import BaseHTTPServer
+import datetime
+
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    '''Handle HTTP requests by returning a fixed 'page'.'''
+
+    # Page to send back.
+    version = '1.0'
+    Page = '''\
+<html>
+<body>
+<p>App version %s</p>
+</body>
+
+</html>
+'''% version
+
+    # Handle a GET request.
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.send_header("Content-Length", str(len(self.Page)))
+        self.end_headers()
+        self.wfile.write(self.Page)
+
+#----------------------------------------------------------------------
+
+if __name__ == '__main__':
+    serverAddress = ('', 8080)
+    server = BaseHTTPServer.HTTPServer(serverAddress, RequestHandler)
+    server.serve_forever()
